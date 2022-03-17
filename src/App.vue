ffc5ece2b764c537eb40ad5af8911624e0a63c1b
eastcopper <ldh7228@gmail.com>
2022-03-17 14:15:26 +0900

    📑 :: (#7)[Git] Git init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div>
+        <h1>TodoList</h1>
+        <input type="text" v-model="userInput" @keyup.enter="setTodoList" />
+        <div class="contents">
+          <li v-for="todo in todoList" v-bind:key="todo">
+            {{ todo.label }}
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { data } from "@/lib/export/data";
+
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      userInput: "",
+      todoList: data,
+    };
+  },
+  methods: {
+    setTodoList() {
+      this.todoList.push({
+        label: this.userInput,
+        state: "active",
+      });
+      this.userInput = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  padding-left: 20px;
+}
+</style>
---
+++ src/components/page/mainPage/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <input />
+  <button>추가</button>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  components: {},
+  props: {},
+};
+</script>
+
+<style scoped></style>
