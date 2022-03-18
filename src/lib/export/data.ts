3a9135ec9bebeaa2fadc85d97138a28f767ccff8
eastcopper <ldh7228@gmail.com>
2022-03-18 19:19:25 +0900

    ⚡ :: (#30)[Export] Add fix

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -2,6 +2,7 @@ type dataType = {
   label: String;
   key: number;
   check: boolean;
+  fix: boolean;
 };
 
 export const data: Array<dataType> = [
@@ -9,15 +10,18 @@ export const data: Array<dataType> = [
     label: "밥 먹기",
     key: 0,
     check: false,
+    fix: false,
   },
   {
     label: "양치하기",
     key: 1,
     check: false,
+    fix: false,
   },
   {
     label: "세수하기",
     key: 2,
     check: false,
+    fix: false,
   },
 ];
