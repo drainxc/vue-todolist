69dffb49b247fb6b5b56761a4a8e7b6778bced05
eastcopper <ldh7228@gmail.com>
2022-03-18 15:52:29 +0900

    ⚡ :: (#18)[MainPage] Add MainPage

--- src/components/page/mainPage/index.vue
+++ src/components/page/mainPage/index.vue
@@ -1,14 +1,59 @@
 <template>
-  <input />
-  <button>추가</button>
+  <div id="app">
+    <div class="container">
+      <div>
+        <h1>TodoList</h1>
+        <input type="text" v-model="userInput" @keyup.enter="setTodoList" />
+        <div class="contents" v-for="todo in todoList" v-bind:key="todo">
+          <li>
+            {{ todo.label }}
+          </li>
+          <button v-on:click="deleteBtn(todo)">delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { data } from "../../../lib/export/data";
+
+@Options({
   name: "MainPage",
   components: {},
-  props: {},
-};
+  data() {
+    return {
+      userInput: "",
+      todoList: data,
+      nextID: 3,
+    };
+  },
+  methods: {
+    setTodoList: function (): void {
+      if (this.userInput !== "") {
+        this.todoList.push({
+          label: this.userInput,
+          state: "active",
+          key: this.nextID,
+        });
+        this.nextID += 1;
+        this.userInput = "";
+      }
+    },
+
+    deleteBtn: function (todo: any): void {
+      this.todoList = this.todoList.filter(
+        (item: any) => item.key !== todo.key
+      );
+    },
+  },
+})
+export default class MainPage extends Vue {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.contents {
+  display: flex;
+}
+</style>
