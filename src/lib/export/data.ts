b70353c239347486c545e0dc56b549d486ec927a
eastcopper <ldh7228@gmail.com>
2022-03-18 15:10:05 +0900

    ⚡ :: (#11)[Export] Add key

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -1,19 +1,23 @@
 type dataType = {
   label: String;
   state: String;
+  key: number;
 };
 
 export const data: Array<dataType> = [
   {
     label: "밥 먹기",
-    state: "",
+    state: "active",
+    key: 0,
   },
   {
     label: "양치하기",
-    state: "",
+    state: "active",
+    key: 1,
   },
   {
     label: "세수하기",
-    state: "",
+    state: "active",
+    key: 2,
   },
 ];
