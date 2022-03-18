41c591675bc75b6110f7fc4b6ef39104edac7962
eastcopper <ldh7228@gmail.com>
2022-03-18 19:06:36 +0900

    ⚡ :: (#29)[Export] Remove State

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -1,6 +1,5 @@
 type dataType = {
   label: String;
-  state: String;
   key: number;
   check: boolean;
 };
@@ -8,19 +7,16 @@ type dataType = {
 export const data: Array<dataType> = [
   {
     label: "밥 먹기",
-    state: "active",
     key: 0,
     check: false,
   },
   {
     label: "양치하기",
-    state: "active",
     key: 1,
     check: false,
   },
   {
     label: "세수하기",
-    state: "active",
     key: 2,
     check: false,
   },
