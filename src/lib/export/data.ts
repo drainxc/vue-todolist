68d6ae5b2797bed5f3e549f0d01b8ca392609def
eastcopper <ldh7228@gmail.com>
2022-03-18 18:56:04 +0900

    ⚡ :: (#27)[Export] Add Check

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -2,6 +2,7 @@ type dataType = {
   label: String;
   state: String;
   key: number;
+  check: boolean;
 };
 
 export const data: Array<dataType> = [
@@ -9,15 +10,18 @@ export const data: Array<dataType> = [
     label: "밥 먹기",
     state: "active",
     key: 0,
+    check: false,
   },
   {
     label: "양치하기",
     state: "active",
     key: 1,
+    check: false,
   },
   {
     label: "세수하기",
     state: "active",
     key: 2,
+    check: false,
   },
 ];
