b0c89bbaa3ffa5a255fe42185fa2d91b33fb54dc
eastcopper <ldh7228@gmail.com>
2022-03-18 18:56:28 +0900

    ⚡ :: (#28)[MainPage] Add CheckBox

--- src/components/common/Contents/index.vue
+++ src/components/common/Contents/index.vue
@@ -2,6 +2,7 @@
   <input type="text" v-model="userInput" @keyup.enter="setTodoList" />
   <div class="contents" v-for="todo in todoList" v-bind:key="todo">
     <li>{{ todo.label }}</li>
+    <input type="checkbox" v-model="todo.check" />
     <button v-on:click="deleteBtn(todo)">delete</button>
   </div>
 </template>
@@ -21,11 +22,13 @@ import { data } from "../../../lib/export/data";
   },
   methods: {
     setTodoList: function (): void {
-      if (this.userInput !== "") { // input 값이 들어있다면
+      if (this.userInput !== "") {
+        // input 값이 들어있다면
         this.todoList.push({
           label: this.userInput,
           state: "active",
           key: this.nextID,
+          cheak: false,
         }); // data 추가
         this.nextID += 1; // key
         this.userInput = ""; // input 값 비우기
