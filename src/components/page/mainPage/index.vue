d9b98208702922f84998dae05796af55932cf9b1
eastcopper <ldh7228@gmail.com>
2022-03-18 16:18:21 +0900

    ⚡ :: (#24)[App] Add Contents

--- src/components/page/mainPage/index.vue
+++ src/components/page/mainPage/index.vue
@@ -3,13 +3,7 @@
     <div class="container">
       <div>
         <h1>TodoList</h1>
-        <input type="text" v-model="userInput" @keyup.enter="setTodoList" />
-        <div class="contents" v-for="todo in todoList" v-bind:key="todo">
-          <li>
-            {{ todo.label }}
-          </li>
-          <button v-on:click="deleteBtn(todo)">delete</button>
-        </div>
+        <Contents />
       </div>
     </div>
   </div>
@@ -17,42 +11,15 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { data } from "../../../lib/export/data";
+import Contents from "../../common/Contents/index.vue";
 
 @Options({
   name: "MainPage",
-  components: {},
-  data() {
-    return {
-      userInput: "",
-      todoList: data,
-      nextID: 3,
-    };
-  },
-  methods: {
-    setTodoList: function (): void {
-      if (this.userInput !== "") {
-        this.todoList.push({
-          label: this.userInput,
-          state: "active",
-          key: this.nextID,
-        });
-        this.nextID += 1;
-        this.userInput = "";
-      }
-    },
-    deleteBtn: function (todo: any): void {
-      this.todoList = this.todoList.filter(
-        (item: any) => item.key !== todo.key
-      );
-    },
+  components: {
+    Contents,
   },
 })
 export default class MainPage extends Vue {}
 </script>
 
-<style scoped>
-.contents {
-  display: flex;
-}
-</style>
+<style scoped></style>
